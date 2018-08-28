7b0da1bcb8423902e560ccef894c7f225ba4eb91
forest-shadow <mamasay@yandex.ru>
2018-08-28 10:16:04 +0300

    Initialize app structure

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, browserHistory } from 'react-router'
+
+import routes from './routes'
+
+import './styles/styles.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+render(
+  <Router history={ browserHistory } routes={ routes } />,
+  document.getElementById('app')
+)
